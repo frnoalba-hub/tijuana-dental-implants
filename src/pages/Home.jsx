823faa1093967dd07e5f3ddcb14a0f75a8f1f5d2
Delta Block <deltablockv@gmail.com>
2026-02-24 06:01:21 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,6 +19,7 @@ export default function Home() {
             <ServicesSection />
             <GallerySection />
             <ExpertiseSection />
+            <VideoSection />
             <WhyTijuanaSection />
             <ContactSection />
             <Footer />
